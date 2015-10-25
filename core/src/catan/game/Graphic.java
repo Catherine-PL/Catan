11fbb42852d5635b7423f6c49dbfdd3846b329d0
Sebastian Katszer <sebastian.katszer@gmail.com>
2015-10-25 17:58:14 +0100

    v1.0

--- core/src/catan/game/Graphic.java
+++ core/src/catan/game/Graphic.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Catan extends ApplicationAdapter {
+public class Graphic extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
--- desktop/src/catan/game/desktop/DesktopLauncher.java
+++ desktop/src/catan/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package catan.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import catan.game.Catan;
+import catan.game.Graphic;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Catan(), config);
+		new LwjglApplication(new Graphic(), config);
 	}
 }
