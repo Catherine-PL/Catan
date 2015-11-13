5f378401c01d83142af8df29e188245d29f6e3a8
Katarzyna <katarzyna.kosiak@o2.pl>
2015-11-13 14:17:30 +0100

    dodanie rysowania dróg

--- core/src/representation/Gameplay.java
+++ core/src/representation/Gameplay.java
@@ -29,6 +29,11 @@ public class Gameplay extends View
 		{
 			return y;
 		}
+		void set(int _x,int _y)
+		{
+			x=_x;
+			y=_y;
+		}
 		
 	}
 	
@@ -44,15 +49,23 @@ public class Gameplay extends View
 	private Texture nobuilding;
 	private Texture village;
 	
+	private Texture roadSWNE;
+	private Texture roadNWSE;
+	private Texture roadSN;
+	
 	private Board board;
 	//tekstury na wszystkie tile
 	private ArrayList<Texture> textures = new ArrayList<Texture>();
 	//tablica na miasta
-	private Coordinates[] buildingsXY = new Coordinates[54];
-	
-
+	private Coordinates[] buildingsXY;
 	public void init()
 	{
+		buildingsXY = new Coordinates[54];
+		for(int i=0;i<54;i++)
+		{
+			buildingsXY[i] = new Coordinates(100,100);
+		}
+
 		batch = new SpriteBatch();
 		board = Board.getInstance();
 		initTiles();
@@ -63,21 +76,22 @@ public class Gameplay extends View
 		city = new Texture(Gdx.files.internal("city.png"));
 		nobuilding = new Texture(Gdx.files.internal("nobuilding.png"));
 		village = new Texture(Gdx.files.internal("village.png"));
+		roadSWNE = new Texture(Gdx.files.internal("roadSWNE.png"));;
+		roadNWSE = new Texture(Gdx.files.internal("roadNWSE.png"));;
+		roadSN = new Texture(Gdx.files.internal("roadNS.png"));;
+		
 	}
 	
 	public void batch()
 	{
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//menu i t³o w czasie rozgrywki
 		batchMenuGameplay();
-		//plansza - kafelki
 		batchTiles();
-		//plansza - miasta
 		batchBuildings();
-		//TODO plansza - drogi
-		
+		batchRoads();
 		batch.end();
 	}	
 	
@@ -120,7 +134,9 @@ public class Gameplay extends View
 		int cX=560;
 		int cY=700;
 		int counter=0;
+		//number of cities in the row
 		int row=3;
+		//12 rows
 		for (int i =0;i<12;i++)
 		{
 			cY=cY-30;
@@ -153,12 +169,71 @@ public class Gameplay extends View
 					batch.draw(city, cX+j*110, cY);
 					break;
 				}
-				buildingsXY[counter] = new Coordinates(cX+j*110, cY);
+				buildingsXY[counter].set(cX+j*110, cY);
 				counter=counter+1;
 			}	
 		}
 	}
 	
+	
+	private void batchRoads()
+	{
+		int x=0;
+		int y=0;
+		int myX=0;
+		int myY=0;
+		
+		for (int i =0;i<54;i++)
+		{				
+			myX=buildingsXY[i].getX();
+			myY=buildingsXY[i].getY();
+			for (Node n : board.getNode(i).getNeighbours())
+			{
+				x=buildingsXY[n.getNodeNumber()].getX();
+				y=buildingsXY[n.getNodeNumber()].getY();
+				
+				
+				if ((x ==myX) || (y>myY))
+				{
+					batch.draw(roadSN, myX, myY);
+				}
+				if ((x ==myX) || (y<myY))
+				{
+					batch.draw(roadSN, myX, y);
+				}
+				if ((x >myX) || (y<myY))
+				{
+					batch.draw(roadNWSE, myX, y);					
+				}
+				if ((x <myX) || (y>myY))
+				{
+					batch.draw(roadNWSE, x, myY);					
+				}
+				if ((x <myX) || (y<myY))
+				{
+					batch.draw(roadSWNE, x, y);					
+				}
+				if ((x >myX) || (y>myY))
+				{
+					batch.draw(roadSWNE, myX, myY);					
+				}
+			}
+			
+			if ((i ==6) || (i==43))
+			{
+				i=i+5;
+			}
+			if ((i ==15) || (i==37))
+			{
+				i=i+6;
+			}
+			if (i ==26)
+			{
+				i=i+7;
+			}
+		}
+		
+	}
 	
 	
 	private void batchTiles()
@@ -171,23 +246,23 @@ public class Gameplay extends View
 		batch.draw(textures.get(1), cX, cY);
 		batch.draw(textures.get(2), cX-tileX+1, cY);
 		batch.draw(textures.get(0), cX+tileX-1, cY);
-		//2rz¹d
+		//2rzÄ…d
 		batch.draw(textures.get(4), cX-tileX/2+1, cY-tileY+tileY/4+2);
 		batch.draw(textures.get(5), cX+tileX/2-1, cY-tileY+tileY/4+2);
 		batch.draw(textures.get(3), cX-tileX/2+2-tileX, cY-tileY+tileY/4+2);
 		batch.draw(textures.get(6), cX+tileX/2-2+tileX, cY-tileY+tileY/4+2);
-		//3rz¹d
+		//3rzÄ…d
 		batch.draw(textures.get(9), cX, cY-2*tileY+2*tileY/4+2);
 		batch.draw(textures.get(8), cX-tileX+1, cY-2*tileY+2*tileY/4+2);
 		batch.draw(textures.get(10), cX+tileX-1, cY-2*tileY+2*tileY/4+2);
 		batch.draw(textures.get(7), cX-2*tileX+2, cY-2*tileY+2*tileY/4+2);
 		batch.draw(textures.get(11), cX+2*tileX-2, cY-2*tileY+2*tileY/4+2);
-		//4rz¹d 
+		//4rzÄ…d 
 		batch.draw(textures.get(13), cX-tileX/2+1, cY-3*tileY+3*tileY/4+3);
 		batch.draw(textures.get(14), cX+tileX/2-1, cY-3*tileY+3*tileY/4+3);
 		batch.draw(textures.get(12), cX-tileX/2+2-tileX, cY-3*tileY+3*tileY/4+3);
 		batch.draw(textures.get(15), cX+tileX/2-2+tileX, cY-3*tileY+3*tileY/4+3);
-		//5rz¹d
+		//5rzÄ…d
 		batch.draw(textures.get(17), cX, cY-4*tileY+4*tileY/4+4);
 		batch.draw(textures.get(16), cX-tileX+1, cY-4*tileY+4*tileY/4+4);
 		batch.draw(textures.get(18), cX+tileX-1, cY-4*tileY+4*tileY/4+4);
@@ -201,7 +276,7 @@ public class Gameplay extends View
 		batch.draw(playerAvatar,0,0);
 		//wyswietlenie paska na zasoby, opcje,miasta,drogi i punkty
 		batch.draw(gameplayMenu,0,0);
-		//wyswietlenie postaci gracza który aktualnie gra
+		//wyswietlenie postaci gracza ktÃ³ry aktualnie gra
 		batch.draw(cubePlayer,1050,400);
 	}
 	
