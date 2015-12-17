fcf6afbf0dd88bee4e363ca668bde5a3d9e47e4d
Pavl0 <pawelogorzaly@gmail.com>
2015-12-17 18:53:36 +0100

    Dice - modyfikacja

--- core/src/database/Dice.java
+++ core/src/database/Dice.java
@@ -3,6 +3,9 @@ package database;
 import java.util.Random;
 
 public class Dice {
+	private int firstValue;
+	private int secondValue;
+	
 	private static class InstanceHolder{
 		private static final Dice instance = new Dice();
 	}
@@ -14,12 +17,25 @@ public class Dice {
 
 	Random generator = new Random();
 	//int result=0;
-	int throDice(){
+	/*int throDice(){
 		//rzut dwoma koœæmi
 		return generator.nextInt(11)+1;
 		//result=generator.nextInt(5)+1+generator.nextInt(5)+1;
 		//return result;
 		//					0-11+1=1-12
 		//return generator.nextInt(11)+1; alternatywa krótsza
+	}*/
+	
+	public void throDice(){
+		firstValue = generator.nextInt(5)+1;
+		secondValue = generator.nextInt(5)+1;
+	}
+	
+	public int getFirst(){
+		return firstValue;
+	}
+	
+	public int getSecond(){
+		return secondValue;
 	}
 }
